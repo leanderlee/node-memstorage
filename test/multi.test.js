8ba7430db0d0ca4eaff47f82c3835637ce5f828f
Leander (Desktop) <me@leander.ca>
2014-01-24 17:31:57 -0500

    Added MultiStore, which allows multiple stores to be written to at the same time.

---
+++ test/multi.test.js
@@ -0,0 +1,79 @@
+var MultiStore = require("../lib/multi");
+var MongoStore = require("../lib/mongo");
+var assert = require("assert");
+
+describe('MultiStorage', function(){
+  describe('#set', function() {
+    it("should insert properly", function (done) {
+      var settings1 = { type: "mongo", settings: { db: "testing", collectionName: "mocha-test-multi-1" } };
+      var settings2 = { type: "mongo", settings: { db: "testing", collectionName: "mocha-test-multi-2" } };
+      var test = new MultiStore({ stores: [settings1, settings2] });
+      var db1 = new MongoStore(settings1.settings);
+      var db2 = new MongoStore(settings2.settings);
+      test.connect(function () {
+        db1.connect(function () {
+          db2.connect(function () {
+            test.set("x", 21, function() {
+              db1.get("x", function (v) {
+                assert.deepEqual(v, [21]);
+                db2.get("x", function (v) {
+                  assert.deepEqual(v, [21]);
+                  done();
+                })
+              })
+            })
+          });
+        });
+      });
+    })
+  });
+  describe('#del', function() {
+    it("should delete properly", function (done) {
+      var settings1 = { type: "mongo", settings: { db: "testing", collectionName: "mocha-test-multi-1" } };
+      var settings2 = { type: "mongo", settings: { db: "testing", collectionName: "mocha-test-multi-2" } };
+      var test = new MultiStore({ stores: [settings1, settings2] });
+      var db1 = new MongoStore(settings1.settings);
+      var db2 = new MongoStore(settings2.settings);
+      test.connect(function () {
+        db1.connect(function () {
+          db2.connect(function () {
+            test.set("x", 21, function() {
+              test.del("x", function() {
+                db1.get("x", function (v) {
+                  assert.deepEqual(v, []);
+                  db2.get("x", function (v) {
+                    assert.deepEqual(v, []);
+                    done();
+                  })
+                })
+              })
+            });
+          });
+        });
+      });
+    })
+  });
+  describe('#get', function() {
+    it("should fetch from the first", function (done) {
+      var settings1 = { type: "mongo", settings: { db: "testing", collectionName: "mocha-test-multi-1" } };
+      var settings2 = { type: "mongo", settings: { db: "testing", collectionName: "mocha-test-multi-2" } };
+      var test = new MultiStore({ stores: [settings1, settings2] });
+      var db1 = new MongoStore(settings1.settings);
+      var db2 = new MongoStore(settings2.settings);
+      test.connect(function () {
+        db1.connect(function () {
+          db2.connect(function () {
+            db1.set("x", 21, function() {
+              db2.set("x", 24, function() {
+                test.get("x", function(v) {
+                  assert.deepEqual(v, [21]);
+                  done();
+                })
+              })
+            });
+          });
+        });
+      });
+    })
+  });
+});
